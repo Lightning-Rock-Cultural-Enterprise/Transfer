8486c57e69b200d01fb5af0fbbc62d2b2a6371c8
timmy61109 <38396747+timmy61109@users.noreply.github.com>
2019-09-25 17:25:12 +0800

    目前測試的參數，理論上應該影片可以對上聲音

--- dshow.bat
+++ dshow.bat
@@ -1,2 +1,2 @@
 ﻿chcp 65001
-ffmpeg -f dshow -i video="HD TV CAM":audio="麥克風 (HD TV CAM)" -audio_buffer_size 500 -max_muxing_queue_size 1024 -init_hw_device cuda -c:v h264_nvenc -c:a mp3 -b:v 9600k -ab 512k -ar 44.1k -r 30 -s 720x480 -aspect 4:3 0726.mp4 -preset veryslow -tune zerolatency
+ffmpeg -f dshow -i video="HD TV CAM":audio="麥克風 (HD TV CAM)" -audio_buffer_size 500 -max_muxing_queue_size 1024 -init_hw_device cuda -c:v h264_nvenc -c:a mp3 -b:v 9600k -ab 512k -ar 44.1k -r 30 -s 720x480 -aspect 4:3 0726.mp4 -pass 2 -preset veryslow -tune zerolatency
