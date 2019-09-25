7461214ab74c4894d0419c3db42a2843e474e9cc
timmy61109 <38396747+timmy61109@users.noreply.github.com>
2019-09-25 14:45:26 +0800

    新增設定檔案

---
+++ dshow.bat
@@ -0,0 +1,2 @@
+﻿chcp 65001
+ffmpeg -f dshow -i video="HD TV CAM":audio="麥克風 (HD TV CAM)" -max_muxing_queue_size 1024 -init_hw_device cuda -c:v h264_nvenc -b:v 4400k -ab 512k -ar 44.1k -r 30 -s 720x480 -aspect 4:3 0733.mp4
