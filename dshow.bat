bc06f87ef577312281c0c1f449478c0a39771e05
timmy61109 <38396747+timmy61109@users.noreply.github.com>
2019-10-04 15:21:31 +0800

    修改成AverMedia C725B可以使用與正常錄影

--- dshow.bat
+++ dshow.bat
@@ -1,3 +1,4 @@
+@echo off
 ﻿chcp 65001
-ffmpeg -f dshow -i video="AVerMedia USB Polaris Analog Capture":audio="Analog Audio In (AVerMedia C039" -max_muxing_queue_size 1024 -init_hw_device cuda -c:v h264_nvenc -c:a aac -b:v 9600k -ab 512k -ar 44.1k -r 29.97 -s 720x480 -aspect 4:3 ../0713.mp4 -pass 2 -preset veryslow -tune zerolatency
+ffmpeg -f dshow -i video="AVerMedia SD Analog Capture":audio="Analog Audio In (AVerMedia C725" -video_size 720x480 -framerate 30 -sample_rate 96000 -sample_size 16 -channels 2 -pixel_format rgb24 -max_muxing_queue_size 1024 -init_hw_device cuda -c:v h264_nvenc -c:a aac -b:v 9600k -ab 1024k -ar 96k -r 30 -s 720x480 -aspect 4:3 ../0713.mp4 -pass 2 -preset veryslow -tune zerolatency
 pause
