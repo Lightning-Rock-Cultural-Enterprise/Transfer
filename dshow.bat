f3c22af6c940a494ffb5ebf3729c2ff2f2cf27e7
timmy61109 <38396747+timmy61109@users.noreply.github.com>
2019-10-15 15:07:42 +0800

    新增兩個暫停並修改檔案位置

--- dshow.bat
+++ dshow.bat
@@ -1,4 +1,5 @@
 @echo off
 ﻿chcp 65001
-ffmpeg -f dshow -i video="AVerMedia SD Analog Capture":audio="Analog Audio In (AVerMedia C725" -video_size 720x480 -framerate 30 -sample_rate 96000 -sample_size 16 -channels 2 -pixel_format rgb24 -max_muxing_queue_size 1024 -init_hw_device cuda -c:v h264_nvenc -c:a aac -b:v 9600k -ab 1024k -ar 96k -r 30 -s 720x480 -aspect 4:3 ../0713.mp4 -pass 2 -preset veryslow -tune zerolatency
+ffmpeg -f dshow -i video="AVerMedia SD Analog Capture":audio="Analog Audio In (AVerMedia C725" -video_size 720x480 -framerate 30 -sample_rate 96000 -sample_size 16 -channels 2 -pixel_format rgb24 -max_muxing_queue_size 1024 -init_hw_device cuda -c:v h264_nvenc -c:a aac -b:v 9600k -ab 1024k -ar 96k -r 30 -s 720x480 -aspect 4:3 ../input/None.mp4 -pass 2 -preset veryslow -tune zerolatency
+pause
 pause
