5d628429192a2af120d27a1708dbb9055ce23131
timmy61109 <38396747+timmy61109@users.noreply.github.com>
2019-10-22 17:11:20 +0800

    將剪輯用的修改成不用解碼與編碼，直接複製加快單純剪輯的速度

--- clip.bat
+++ clip.bat
@@ -1,1 +1,1 @@
-ffmpeg -i ..\input\0725.mp4 -c:v libx264 -c:a aac -b:v 4400k -ab 1024k -ar 96k -r 30 -s 720x480 -aspect 4:3 ../None.mp4 -pass 2 -preset veryslow -tune zerolatency
+ffmpeg -i ..\input\0724.mp4 -vf yadif=mode=0:parity=auto:deint=0 -c:v copy -c:a copy -ss 00:00:00 -t 01:00:00 ../None.mp4
