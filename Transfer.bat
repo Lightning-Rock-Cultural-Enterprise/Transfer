cf31bfa1d1324916c284fe1d58045ab078b15066
timmy61109 <38396747+timmy61109@users.noreply.github.com>
2019-10-22 17:13:27 +0800

    讓它可以使用顯示卡運作，並作為轉檔與剪輯一起使用

--- Transfer.bat
+++ Transfer.bat
@@ -1,1 +1,1 @@
-ffmpeg -init_hw_device cuda -c:v  h264_cuvid -hwaccel cuda -i input\0725.mp4 -init_hw_device cuda -c:v h264_nvenc -b:v 4400k -ab 512k -ar 48000 -r 30 -s 720x480 -aspect 4:3 output\0725.mp4
+ffmpeg -i ..\input\0724.mp4 -vf yadif=mode=0:parity=auto:deint=0 -init_hw_device cuda -c:v h264_nvenc -c:a aac -b:v 9600k -ab 1024k -ar 96k -r 30 -s 720x480 -aspect 4:3 -ss 00:01:00 -t 00:10:00 ..\None.mp4 -pass 2 -preset veryslow -tune zerolatency
