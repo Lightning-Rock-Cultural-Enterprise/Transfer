431048f68fd447740b55598c9c2c1be988854729
timmy61109 <38396747+timmy61109@users.noreply.github.com>
2019-10-24 15:00:23 +0800

    修改成可以使用GPU並剪輯影片

--- Transfer.bat
+++ Transfer.bat
@@ -1,1 +1,2 @@
-ffmpeg -i ..\input\0724.mp4 -vf yadif=mode=0:parity=auto:deint=0 -init_hw_device cuda -c:v h264_nvenc -c:a aac -b:v 9600k -ab 1024k -ar 96k -r 30 -s 720x480 -aspect 4:3 -ss 00:01:00 -t 00:10:00 ..\None.mp4 -pass 2 -preset veryslow -tune zerolatency
+chcp 65001
+ffmpeg -hwaccel cuvid -i ../None.mp4 -threads 20 -vf yadif=mode=0:parity=auto:deint=0 -init_hw_device cuda -c:v h264_nvenc -c:a aac -b:v 4400k -ab 1024k -ar 96k -r 30 -s 720x480 -aspect 4:3 -ss 00:00:08.28 -to 01:18:29.18 ..\None.mp4 -pass 2 -preset veryslow -tune film
