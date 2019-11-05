d8da71d3c57969bae2e6756dd91b5855b6a0f425
timmy61109 <38396747+timmy61109@users.noreply.github.com>
2019-11-05 14:46:16 +0800

    此提交將新增可以控制選擇顯示卡的參數，但好像沒有用，需要再研究

--- Transfer.bat
+++ Transfer.bat
@@ -1,2 +1,3 @@
 chcp 65001
-ffmpeg -hwaccel cuvid -i ../None.mp4 -threads 20 -vf yadif=mode=0:parity=auto:deint=0 -init_hw_device cuda -c:v h264_nvenc -c:a aac -b:v 4400k -ab 1024k -ar 96k -r 30 -s 720x480 -aspect 4:3 -ss 00:00:08.28 -to 01:18:29.18 ..\None.mp4 -pass 2 -preset veryslow -tune film
+ffmpeg -hwaccel cuvid -hwaccel_device 0 -i None.mp4 -vf yadif=mode=0:parity=auto:deint=0 -init_hw_device cuda -c:v h264_nvenc -c:a aac -b:v 4400k -ab 1024k -ar 96k -r 30 -s 720x480 -aspect 4:3 -ss 00:00:00.00 -to 00:00:00.00 None.mp4 -pass 2 -preset veryslow -tune film
+pause
