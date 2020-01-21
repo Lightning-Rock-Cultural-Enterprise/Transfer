78b53ab523dca144b65b0504c0426687cb13ef65
timmy61109 <38396747+timmy61109@users.noreply.github.com>
2020-01-21 18:20:14 +0800

    如果採用，此提交將刪除播放音樂功能，並可以重複10次執行

--- copyDVD.bat
+++ copyDVD.bat
@@ -1,5 +1,49 @@
 mkdir .\VIDEO_TS\VIDEO_TS\
 copy /v G:\VIDEO_TS\*.* .\VIDEO_TS\VIDEO_TS\
 7z h -scrcsha256 .\VIDEO_TS\VIDEO_TS
-Somewhere_Out_There.wav
+pause
+
+mkdir .\VIDEO_TS\VIDEO_TS\
+copy /v G:\VIDEO_TS\*.* .\VIDEO_TS\VIDEO_TS\
+7z h -scrcsha256 .\VIDEO_TS\VIDEO_TS
+pause
+
+mkdir .\VIDEO_TS\VIDEO_TS\
+copy /v G:\VIDEO_TS\*.* .\VIDEO_TS\VIDEO_TS\
+7z h -scrcsha256 .\VIDEO_TS\VIDEO_TS
+pause
+
+mkdir .\VIDEO_TS\VIDEO_TS\
+copy /v G:\VIDEO_TS\*.* .\VIDEO_TS\VIDEO_TS\
+7z h -scrcsha256 .\VIDEO_TS\VIDEO_TS
+pause
+
+mkdir .\VIDEO_TS\VIDEO_TS\
+copy /v G:\VIDEO_TS\*.* .\VIDEO_TS\VIDEO_TS\
+7z h -scrcsha256 .\VIDEO_TS\VIDEO_TS
+pause
+
+mkdir .\VIDEO_TS\VIDEO_TS\
+copy /v G:\VIDEO_TS\*.* .\VIDEO_TS\VIDEO_TS\
+7z h -scrcsha256 .\VIDEO_TS\VIDEO_TS
+pause
+
+mkdir .\VIDEO_TS\VIDEO_TS\
+copy /v G:\VIDEO_TS\*.* .\VIDEO_TS\VIDEO_TS\
+7z h -scrcsha256 .\VIDEO_TS\VIDEO_TS
+pause
+
+mkdir .\VIDEO_TS\VIDEO_TS\
+copy /v G:\VIDEO_TS\*.* .\VIDEO_TS\VIDEO_TS\
+7z h -scrcsha256 .\VIDEO_TS\VIDEO_TS
+pause
+
+mkdir .\VIDEO_TS\VIDEO_TS\
+copy /v G:\VIDEO_TS\*.* .\VIDEO_TS\VIDEO_TS\
+7z h -scrcsha256 .\VIDEO_TS\VIDEO_TS
+pause
+
+mkdir .\VIDEO_TS\VIDEO_TS\
+copy /v G:\VIDEO_TS\*.* .\VIDEO_TS\VIDEO_TS\
+7z h -scrcsha256 .\VIDEO_TS\VIDEO_TS
 pause
